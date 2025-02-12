68a8ed815d7051eba93b339608623c84faa374de
smirnov-mi <smirnov.mi@gmail.com>
2025-02-12 19:10:33 +0100

    output plus

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,7 @@ output "server_public_ips" {
   value       = hcloud_server.ubuntu_server[*].ipv4_address
 }
 
+output "add_text_server_public_ips" {
+  description = "how to print Public IPs of the created servers"
+  value =  " RUN   terraform output -json server_public_ips | jq -r '.[]'  to get a list of new Public IPs"
+}
